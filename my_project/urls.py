383b2fbd4ae033b1c431db39e7c92e359b2cb511
teddylindstrom <teddy.lindstrom.work@gmail.com>
2024-08-22 17:03:27 +0000

    added 'path('about/', about_views.about_me, name='about'),' to the my_project urls.py file

--- my_project/urls.py
+++ my_project/urls.py
@@ -21,5 +21,6 @@ from about import views as about_views
 
 urlpatterns = [
     path('hello/', index_views.index, name='index'),
+    path('about/', about_views.about_me, name='about'),
     path('admin/', admin.site.urls),
 ]
