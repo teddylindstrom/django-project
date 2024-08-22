bae13b7d4a16ea63541f42d01c8a83af6b901f77
teddylindstrom <teddy.lindstrom.work@gmail.com>
2024-08-22 16:58:49 +0000

    added 'from about import views as about_views' to the myproject urls.py file

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hello_world import views as index_views
+from about import views as about_views
 
 urlpatterns = [
     path('hello/', index_views.index, name='index'),
