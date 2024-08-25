7a785eac40d5881b89b3c176ef521c6de53a98b1
teddylindstrom <teddy.lindstrom.work@gmail.com>
2024-08-25 14:52:15 +0000

    added to the urls.py 'from django.urls import path', 'from . import views' and 'path('hello/', views.hello_view, name='hello'),' and saying my prayers

--- my_project/urls.py
+++ my_project/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from hello_world import views as index_views
 from about import views as about_views
+from django.urls import path
+from . import views
 
 urlpatterns = [
     path('hello/', index_views.index, name='index'),
     path('about/', about_views.about_me, name='about'),
     path('admin/', admin.site.urls),
+    path('hello/', views.hello_view, name='hello'),
 ]
