7cd32083a418a6905fd63489097fdf30e5cab48d
teddylindstrom <teddy.lindstrom.work@gmail.com>
2024-08-26 14:58:48 +0000

    updated the views.py file with code that is supoosed to work

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-if request.method == "POST":
+def index(request):
+    if request.method == "POST":
         return HttpResponse("You must have POSTed something")
     else:
         return HttpResponse(request.method)
